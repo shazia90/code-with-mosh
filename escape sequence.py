8f50b0d06ccf075ab8d3ea36057e64cb2bbf3c23
shazia90 <61934410+shazia90@users.noreply.github.com>
2020-12-06 02:01:16 +0530

    Add files via upload

---
+++ escape sequence.py
@@ -0,0 +1,17 @@
+# \ "
+# \'
+# \\
+# \n new line
+
+course ='python \" programming'
+
+
+course ='python \' programming'
+
+
+course ='python \\ programming'
+
+
+course ='python \n programming'
+
+print (course)
