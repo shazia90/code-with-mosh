5b9c2b7ee9348e89b6d4795c7a7dac6036f66c51
shazia90 <61934410+shazia90@users.noreply.github.com>
2020-12-06 23:57:46 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,13 @@
+print (10 + 3)
+print (10 - 3)
+print (10 * 3)
+print (10 / 3) #which gives float no
+print (10 // 3)# this gives integer
+print (10 % 3)#modulus remainder of division
+print (10 ** 3)#10 power 3
+
+x = 10
+x = x + 3 # a)
+x += 3 # b) a, b both are same
+print (x)
+
